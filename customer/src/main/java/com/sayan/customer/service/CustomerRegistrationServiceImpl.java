4fd58a7c51ebfbab429489f2f0928f5d62e09417
teamsayan <sayandas2310@yahoo.in>
2021-12-12 16:42:29 +0530

    adding postgres again

--- customer/src/main/java/com/sayan/customer/service/CustomerRegistrationServiceImpl.java
+++ customer/src/main/java/com/sayan/customer/service/CustomerRegistrationServiceImpl.java
@@ -2,12 +2,18 @@ package com.sayan.customer.service;
 
 import com.sayan.customer.dto.CustomerRegisterRequest;
 import com.sayan.customer.model.Customer;
+import com.sayan.customer.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
+@AllArgsConstructor
 @Service
 public class CustomerRegistrationServiceImpl implements CustomerRegistrationService{
+
+    private CustomerRepository customerRepository;
+
     @Override
     public void registerCustomer(CustomerRegisterRequest registerRequest) {
         Customer customer = Customer.builder()
@@ -15,5 +21,8 @@ public class CustomerRegistrationServiceImpl implements CustomerRegistrationServ
                 .lastName(registerRequest.lastName())
                 .email(registerRequest.email())
                 .build();
+
+        Customer created = customerRepository.save(customer);
+        log.info("Created Customer: {}",created.getId());
     }
 }
